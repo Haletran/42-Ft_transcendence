33d1887958eb7ff18530ceadfc9f3cae11cb15d2
ANOUK <ANOUK@gmail.com>
2024-11-14 15:44:15 +0100

    feat: connexion to django through proxy and kong, clicking on register does not forward the request to kong, error 404

--- src/django-credentials/credentials/settings.py
+++ src/django-credentials/credentials/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!2#j8c0or)@3y3(g13=n%3ba%g#jzh$edcmz%@wucvb^sjfiu5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-credentials']
 
 
 # Application definition
