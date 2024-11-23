075ffdd568ee0a2be2ca1cc930c92c1e8db2933a
dboire <dorian.boire98@gmail.com>
2024-11-23 12:11:05 +0100

    Feat : Crypto docker hosting hardhat launching weel, getting init credentials from Vault

--- src/crypto/hardhat.config.js
+++ src/crypto/hardhat.config.js
@@ -1,6 +1,44 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+const axios = require('axios');
+require('dotenv').config(); // Load environment variables from .env file
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.27",
-};
+async function getVaultSecrets() {
+  try {
+    const response = await axios.get('http://vault:8200/v1/secret/data/metamask', {
+      headers: {
+        'X-Vault-Token': process.env.VAULT_TOKEN // Ensure VAULT_TOKEN is set in your environment
+      }
+    });
+    const data = response.data.data.data;
+    return {
+      url: data.url,
+      accounts: [data.account]
+    };
+  } catch (error) {
+    console.error('Error fetching secrets from Vault:', error);
+    return {
+      url: 'l', // Provide a default URL or handle it as needed
+      accounts: [''] // Provide a default account or handle it as needed
+    };
+  }
+}
+
+async function getConfig() {
+  const vaultSecrets = await getVaultSecrets();
+
+  return {
+    solidity: "0.8.27",
+    defaultNetwork: "sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: vaultSecrets.url,
+        accounts: vaultSecrets.accounts
+      }
+    }
+  };
+}
+
+module.exports = (async () => {
+  return await getConfig();
+})();
