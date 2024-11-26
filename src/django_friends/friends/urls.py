fcc8dd16bedbb3b258b305996139b7813e1cb651
dboire <dorian.boire98@gmail.com>
2024-11-26 19:04:02 +0100

    feat: remove debug email printing endpoint from URLs; streamline friend-related routes Adding friend truly working

--- src/django_friends/friends/urls.py
+++ src/django_friends/friends/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import add_friend, print_all_emails, fetch_emails_from_credentials
+from .views import add_friend, fetch_emails_from_credentials
 
 urlpatterns = [
     path('add/', add_friend, name='add_friend'),
-    path('debug/emails/', print_all_emails, name='print_all_emails'),
     path('fetch_emails/', fetch_emails_from_credentials, name='fetch_emails_from_credentials'),
 ]
