b6d0306e42513a522db854752bd09d32c3a43598
Haletran <bpasquier123@gmail.com>
2025-01-06 14:26:43 +0100

    add: script to replace all localhost to access the site outside the VM

---
+++ replace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CURRENT_IP=$(hostname -I | awk '{print $1}')
+SEARCH_DIR="."
+
+if [ $# -eq 1 ]; then
+    SEARCH_DIR="$1"
+fi
+
+echo "Replacing 'localhost' with '$CURRENT_IP' in files under '$SEARCH_DIR'..."
+find "$SEARCH_DIR" -type f ! -name "$(basename "$0")" -exec sed -i "s/localhost/$CURRENT_IP/g" {} +
+echo "Replacement completed!"
