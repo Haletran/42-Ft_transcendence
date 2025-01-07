9352a5f0874086c40dd34a9f32d143f962b7dfdf
Haletran <bpasquier123@gmail.com>
2025-01-07 17:32:24 +0100

    fix: grid instead of item by item

--- src/reverseproxy/static-content/static/spa/pong.js
+++ src/reverseproxy/static-content/static/spa/pong.js
@@ -11,122 +11,126 @@ export class Pong extends Page {
     constructor() {
         super();
         this.template = `
-                    <div class="header">
+                <div class="header">
         <nav class="navbar bg-dark  border-body" data-bs-theme="dark">
             <div class="container-fluid">
-                <a class="navbar-brand " href="/home" data-link="/home">
-                    <img src="/static/imgs/logo.png" alt="" width="25" class="d-inline-block align-text-top invert">
-                    <p class="d-inline montserrat-bold">Ft_transcendence</p>
-                </a>
-                <a class="nav-link active" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img alt="logo_profile_picture" width="40" height="40" class="rounded-circle" style="
-                              object-fit: cover;
-                            " src=""
-                        alt="profile_picture" />
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end">
-                    <li>
-                        <a class="dropdown-item" href="/profile" data-link="/profile">Profile</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="/settings" data-link="/settings" >Settings</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="/friends" data-link="/friends" >Friends</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item" href="/privacy" data-link="/privacy" >Privacy</a>
-                    </li>
-                    <li>
-                        <a class="dropdown-item fw-bold text-danger" href="/" data-link="/" id="logout-butt"><i class="bi bi-box-arrow-left"></i> Logout</a>
-                    </li>
-                </ul>
+            <a class="navbar-brand " href="/home" data-link="/home">
+                <img src="/static/imgs/logo.png" alt="" width="25" class="d-inline-block align-text-top invert">
+                <p class="d-inline montserrat-bold">Ft_transcendence</p>
+            </a>
+            <a class="nav-link active" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img alt="logo_profile_picture" width="40" height="40" class="rounded-circle" style="
+                      object-fit: cover;
+                    " src=""
+                alt="profile_picture" />
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end">
+                <li>
+                <a class="dropdown-item" href="/profile" data-link="/profile">Profile</a>
+                </li>
+                <li>
+                <a class="dropdown-item" href="/settings" data-link="/settings" >Settings</a>
+                </li>
+                <li>
+                <a class="dropdown-item" href="/friends" data-link="/friends" >Friends</a>
+                </li>
+                <li>
+                <a class="dropdown-item" href="/privacy" data-link="/privacy" >Privacy</a>
+                </li>
+                <li>
+                <a class="dropdown-item fw-bold text-danger" href="/" data-link="/" id="logout-butt"><i class="bi bi-box-arrow-left"></i> Logout</a>
+                </li>
+            </ul>
             </div>
         </nav>
-    </div>
-    <div class="menu">
+        </div>
+        <div class="menu">
         <div class="container-fluid d-flex justify-content-center align-items-center center">
             <div id="menu" class="d-flex flex-column align-items-center gap-2">
-                <button class="btn btn-outline-light me-auto" href="/home" data-link="/home"><i
-                        class="bi bi-arrow-left"></i></button>
-                <div id="logo" style="display: flex; align-items: center">
-                    <h1 id="menu" class="display-1 montserrat-bold fw-bold mx-auto">PONG</h1>
+            <button class="btn btn-outline-light me-auto" href="/home" data-link="/home"><i
+                class="bi bi-arrow-left"></i></button>
+            <div id="logo" style="display: flex; align-items: center">
+                <h1 id="menu" class="display-1 montserrat-bold fw-bold mx-auto">PONG</h1>
+            </div>
+            <article class="d-flex flex-column gap-2">
+                <h2 id="menu" class="display-6 montserrat-bold">Choose your game mode</h2>
+                <button id="start_button" value="pvp" class="btn btn-outline-light full-width btn-lg">1
+                vs 1</button>
+                <button id="start_button2" value="vsa" class="btn btn-outline-light full-width btn-lg">1
+                vs AI</button>
+                <button id="settings_button" class="btn btn-outline-info full-width btn-lg">
+                    <i class="bi bi-gear-fill"></i> Settings
+                </button>
+                <div class="modal fade" id="settingsModal" tabindex="-1" aria-labelledby="settingsModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="settingsModalLabel">Game Settings</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                        <div class="col mb-3">
+                            <label for="playerColor" class="form-label">Player Color</label>
+                            <input type="color" class="form-control form-control-color" id="playerColor" value="#ffffff">
+                        </div>
+                        <div class="col mb-3">
+                            <label for="ballColor" class="form-label">Ball Color</label>
+                            <input type="color" class="form-control form-control-color" id="ballColor" value="#000000">
+                        </div>
+                        </div>
+                        <div class="row">
+                        <div class="col mb-3">
+                            <label for="mapColor" class="form-label">Map Color</label>
+                            <input type="color" class="form-control form-control-color" id="mapColor" value="#000000">
+                        </div>
+                        <div class="col mb-3">
+                            <label for="textColor" class="form-label">Text Color</label>
+                            <input type="color" class="form-control form-control-color" id="textColor" value="#000000">
+                        </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" id="resetSettings">RESET</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" id="saveSettings">Save changes</button>
+                    </div>
+                    </div>
                 </div>
-                <article class="d-flex flex-column gap-2">
-                    <h2 id="menu" class="display-6 montserrat-bold">Choose your game mode</h2>
-                    <button id="start_button" value="pvp" class="btn btn-outline-light full-width btn-lg">1
-                        vs 1</button>
-                    <button id="start_button2" value="vsa" class="btn btn-outline-light full-width btn-lg">1
-                        vs AI</button>
-                        <button id="settings_button" class="btn btn-outline-info full-width btn-lg">
-                            <i class="bi bi-gear-fill"></i> Settings
-                        </button>
-                    <div class="modal fade" id="settingsModal" tabindex="-1" aria-labelledby="settingsModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="settingsModalLabel">Game Settings</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="mb-3">
-                                        <label for="playerColor" class="form-label">Player Color</label>
-                                        <input type="color" class="form-control form-control-color" id="playerColor" value="#ffffff">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="ballColor" class="form-label">Ball Color</label>
-                                        <input type="color" class="form-control form-control-color" id="ballColor" value="#000000">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="mapColor" class="form-label">Map Color</label>
-                                        <input type="color" class="form-control form-control-color" id="mapColor" value="#000000">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="textColor" class="form-label">Text Color</label>
-                                        <input type="color" class="form-control form-control-color" id="textColor" value="#000000">
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-danger" id="resetSettings">RESET</button>
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary" id="saveSettings">Save changes</button>
-                                </div>
-                            </div>
+                </div>
+                <div class="accordion" id="accordionExample">
+                <div class="accordion-item text-align-center">
+                  <h2 class="accordion-header">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <h3 id="menu" ><i class="bi bi-trophy-fill"></i> Tournament</h3>
+                    </button>
+                  </h2>
+                  <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                    <div class="d-flex flex-column gap-2">
+                        <label for="customRange1" class="form-label">How many players ?</label>
+                        <div class="d-flex justify-content-center gap-2">
+                        <input type="range" class="form-range" min="4" max="8" step="4" id="customRange1"><span id="rangeValue"></span>
                         </div>
-                    </div>
-                    <div class="accordion" id="accordionExample">
-                        <div class="accordion-item text-align-center">
-                          <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            <h3 id="menu" ><i class="bi bi-trophy-fill"></i> Tournament</h3>
-                            </button>
-                          </h2>
-                          <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-                            <div class="accordion-body">
-                                <div class="d-flex flex-column gap-2">
-                                    <label for="customRange1" class="form-label">How many players ?</label>
-                                    <div class="d-flex justify-content-center gap-2">
-                                        <input type="range" class="form-range" min="4" max="8" step="4" id="customRange1"><span id="rangeValue"></span>
-                                    </div>
-                                        <div class="user_name d-flex flex-column gap-2">
-                                            <input type="text" class="form-control" id="player_1" placeholder="Player 1">
-                                            <input type="text" class="form-control" id="player_2" placeholder="Player 2">
-                                            <input type="text" class="form-control" id="player_2" placeholder="Player 2">
-                                            <input type="text" class="form-control" id="player_2" placeholder="Player 4">
-                                        </div>
-                                    <button id="tournament_button" value="tour" class="btn btn-light">Start Tournament</button>
-                                </div>
-                            </div>
-                          </div>
+                        <div class="user_name d-flex flex-column gap-2">
+                            <input type="text" class="form-control" id="player_1" placeholder="Player 1">
+                            <input type="text" class="form-control" id="player_2" placeholder="Player 2">
+                            <input type="text" class="form-control" id="player_2" placeholder="Player 2">
+                            <input type="text" class="form-control" id="player_2" placeholder="Player 4">
                         </div>
+                        <button id="tournament_button" value="tour" class="btn btn-light">Start Tournament</button>
                     </div>
-                </article>
+                    </div>
+                  </div>
+                </div>
+                </div>
+            </article>
             </div>
         </div>
-    </div>
-    <div id="test" class="game justify-content-center align-items-center position-relative">
+        </div>
+        <div id="test" class="game justify-content-center align-items-center position-relative">
         <button id="pause_button" class="btn btn-outline-light position-absolute position-absolute top-0 start-50 translate-middle"><i class="bi bi-pause-fill"></i></button>
-    </div>
+        </div>
         `;
     }
 
