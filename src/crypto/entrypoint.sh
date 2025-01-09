7645d94dd2e748b8623ed18c963698674eec5e43
dboire <dorian.boire98@gmail.com>
2025-01-09 15:42:05 +0100

    Feat : Increase wait time for Hardhat node startup

--- src/crypto/entrypoint.sh
+++ src/crypto/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+sleep 20
 # Start Hardhat node in the background
 nohup npx hardhat node &
 
 # Wait for Hardhat node to fully start (adjust sleep time as necessary)
-sleep 10
 
 # Run your deploy command
 npx hardhat run scripts/deploy.js --network localhost
